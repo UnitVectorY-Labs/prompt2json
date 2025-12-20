d9866e0da63809abda63024ce3b0d29c441181a6
Jared Hatfield <jjhatf02@gmail.com>
2025-12-20 07:17:03 -0500

    Add initial project files and workflows for Go application

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/UnitVectorY-Labs/prompt2json
+
+go 1.25.5 // GOVERSION
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Place holder for future code
+	fmt.Println("Hello, World!")
+}
